2ee481295c55cfc879bb1f03609745cbb252d3ff
DevSolidloper <139013875+DevSolidloper@users.noreply.github.com>
2023-10-17 08:23:20 -0500

    God is good

---
+++ Ejercicio_4_7/src/animales/gato.java
@@ -0,0 +1,17 @@
+package animales;
+
+
+public class gato extends felino{
+	public String getSonido() {
+		return "Maullido";
+}
+	public String getAlimentos() {
+		return "Ratones";
+}
+	public String getHabitat(){
+		return "Domestico";
+}
+	public String getNombreCientífico() {
+		return "Felis silvestris catus";
+}
+}
---
+++ Ejercicio_4_7/src/animales/perro.java
@@ -0,0 +1,16 @@
+package animales;
+
+public class perro extends Canido {
+	public String getSonido() {
+		return "Ladrido";
+	}
+	public String getAlimentos() {
+		return "Carnivoro";
+	}
+	public String getHabitat(){
+		return "Domestico";
+	}
+	public String getNombreCientífico() {
+		return "Canis lupus familiaris";
+	}
+}
